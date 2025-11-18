75b5f47ed1312bff0648d8102537015a6b373fea
openwj <openwj@hotmail.com>
2025-11-18 15:05:21 +0800

    fix(iam): 修复用户缓存空结果问题
    为getById和getByUsername方法添加unless条件，避免缓存null结果

--- tagtag/tagtag-module/tagtag-module-iam/src/main/java/dev/tagtag/module/iam/service/impl/UserServiceImpl.java
+++ tagtag/tagtag-module/tagtag-module-iam/src/main/java/dev/tagtag/module/iam/service/impl/UserServiceImpl.java
@@ -47,7 +47,7 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements Us
     /** 获取用户详情 */
     @Override
     @org.springframework.transaction.annotation.Transactional(readOnly = true)
-    @Cacheable(cacheNames = "userById", key = "#root.args[0]")
+    @Cacheable(cacheNames = "userById", key = "#root.args[0]", unless = "#result == null")
     public UserDTO getById(Long id) {
         User entity = super.getById(id);
         UserDTO dto = userMapperConvert.toDTO(entity);
@@ -118,7 +118,7 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements Us
      */
     @Override
     @Transactional(readOnly = true)
-    @Cacheable(cacheNames = "userByUsername", key = "#root.args[0]")
+    @Cacheable(cacheNames = "userByUsername", key = "#root.args[0]", unless = "#result == null")
     public UserDTO getByUsername(String username) {
         if (username == null || username.isEmpty()) {
             return null;
